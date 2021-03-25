95ea397326674d448f3ac39f45ff4ef80b56fc7c
ZERO-A-ONE <suyuchenglz@163.com>
2021-03-25 21:56:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo apt-get install libtool-bin bison python3 python3-pip python3-dev git libss
 sudo apt-get install build-essential gcc-multilib debootstrap debian-archive-keyring -y
 sudo apt-get install pkg-config libglib2.0-dev libmount-dev python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential autoconf automake libfreetype6-dev libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev unzip cmake yasm libx264-dev libmp3lame-dev libopus-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev -y
 sudo apt-get build-dep qemu  -y
-sudo apt-get install libgtk2.0-dev re2c -y
+sudo apt-get install libgtk2.0-dev re2c libc6-dev libc6-dev-i386 gcc-multilib g++-multilib clang cmake -y
 git clone git://github.com/ninja-build/ninja.git && cd ninja
 ./configure.py --bootstrap
 sudo cp ninja /usr/bin/
@@ -15,5 +15,6 @@ pip3 install IPython
 pip3 install angr
 pip3 install git+https://github.com/angr/tracer
 pip3 install git+https://github.com/shellphish/driller
-sudo echo core | sudo tee /proc/sys/kernel/core_patter
-sudo echo 1 | sudo tee /proc/sys/kernel/sched_child_runs_firs
+sudo apt-get install -y build-essential python3-dev automake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools
+sudo apt-get install -y lld llvm llvm-dev clang 
+sudo apt-get install -y gcc-$(gcc --version|head -n1|sed 's/.* //'|sed 's/\..*//')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/.* //'|sed 's/\..*//')-dev
